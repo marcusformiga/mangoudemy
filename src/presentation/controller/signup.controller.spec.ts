bc86fe55e097b3e6064992f26129e3510b172cd9
marcus formiga <kiko.antares@gmail.com>
2022-01-07 13:43:29 -0300

    refactor: create interface controller

--- src/presentation/controller/signup.controller.spec.ts
+++ src/presentation/controller/signup.controller.spec.ts
@@ -28,4 +28,18 @@ describe("SignUpController", () =>{
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError("email"))
   })
+  it("Should return 400 if no password has provided", () =>{
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: "anyname@mail.com",
+        email: "anyemail@mail.com",
+        passwordConfirmation: "anypass"
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError("password"))
+  })
+  
 })
--- src/presentation/controller/signup.controller.ts
+++ src/presentation/controller/signup.controller.ts
@@ -1,13 +1,15 @@
-import { MissingParamError } from "../errors/missing-param-error"
-import { badRequest } from "../helpers/bad-request"
-import { HttpRequest, HttpResponse } from "../protocols/http"
+import { MissingParamError } from "../errors/missing-param-error";
+import { badRequest } from "../helpers/bad-request";
+import { Controller } from "../protocols/controller";
+import { HttpRequest, HttpResponse } from "../protocols/http";
 
-export class SignUpController {
-  handle(httpRequest: HttpRequest): HttpResponse {
-    if(!httpRequest.body.name)
-    return badRequest(new MissingParamError("name"))
-    if(!httpRequest.body.email){
-      return badRequest(new MissingParamError("email"))
+export class SignUpController implements Controller {
+  handle(httpRequest: HttpRequest): HttpResponse{
+    const requiredFields = ["name", "email", "password"]
+    for(let field of requiredFields){
+      if(!httpRequest.body[field]){
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
